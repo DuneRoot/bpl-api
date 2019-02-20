93b654ae6cab82019a0413fd9811244406f16bec
Alistair <johnyob132@gmail.com>
2019-02-20 19:06:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     name="bpl-api",
     version="0.1.0",
     author="Alistair O'Brien",
-    author_email="alistair.o'brien@ellesmere.com",
+    author_email="alistair@duneroot.co.uk",
     description="An API for the Blockpool Blockchain.",
     long_description=long_description,
     long_description_content_type="text/markdown",
